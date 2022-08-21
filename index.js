2cba664e150a9c65bc5099f3eb75e889f545c22f
artfront5 <art.front@list.ru>
2022-08-21 11:22:53 +0300

    Yandex map

--- index.js
+++ index.js
@@ -23,3 +23,30 @@
 //     section.classList.remove('hidden-section');
 //   }
 // });
+let center = [60.075995987961484, 30.339222559444504];
+
+function init() {
+  let map = new ymaps.Map('map-test', {
+    center: center,
+    zoom: 17
+  });
+
+let placemark = new ymaps.Placemark(center, {}, {
+  // iconLayout: 'default#image',
+  // iconImageHref: 'img/placeholder.png',
+  // iconImageSize: [30, 30],
+  // iconImageOffset: [5, -40]
+});
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  map.geoObjects.add(placemark);
+
+}
+ymaps.ready(init)
